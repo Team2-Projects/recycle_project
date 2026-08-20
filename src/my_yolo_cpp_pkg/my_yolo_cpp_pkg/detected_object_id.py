c2aae9a555477391a608d8b815a0a377b43e54d7
HanSeungHee2 <tmdgml88888@naver.com>
2026-08-20 12:21:04 +0900

    multi_recycle logic

--- src/my_yolo_cpp_pkg/my_yolo_cpp_pkg/detected_object_id.py
+++ src/my_yolo_cpp_pkg/my_yolo_cpp_pkg/detected_object_id.py
@@ -19,15 +19,13 @@ class YoloNode(Node):
 
         self.is_tracking = False # 추적 모드 플래그
         self.declare_parameter('conf_threshold', 0.4)
-<<<<<<< Updated upstream
-        self.model = YOLO('/home/user/turtlebot3_ws/src/my_yolo_cpp_pkg/models/transfer_v3_openvino_model')
-=======
+
         self.model = YOLO('/home/hee/turtlebot3_ws/src/my_yolo_cpp_pkg/models/transfer_v3_openvino_model')
         
         # [시간 측정용 변수 초기화]
         self.processing_times = []
         
->>>>>>> Stashed changes
+
         # 모델 경로를 확인하세요
         # 1. 패키지의 share 경로를 자동으로 찾음
         # package_share_directory = get_package_share_directory('my_yolo_cpp_pkg')
@@ -95,14 +93,17 @@ class YoloNode(Node):
                 msg_data.id = object_id[best_name]
                 msg_data.confidence = confidences[target_idx]
                 msg_data.coord = [float(x) for x in best_coord]
+                msg_data.max_y_up = best_coord[1] - 0.5*best_coord[3]
             else:
                 msg_data.id = -1
                 msg_data.confidence = 0.0
                 msg_data.coord = [0.0, 0.0, 0.0, 0.0]
+                msg_data.max_y_up = 0
         else:
             msg_data.id = -1
             msg_data.confidence = 0.0
             msg_data.coord = [0.0, 0.0, 0.0, 0.0]
+            msg_data.max_y_up  = 0
 
         self.publisher_.publish(msg_data)
         end_time = time.time()
